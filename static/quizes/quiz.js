cefe358307acc8f5e30750037e585fff2f2f0d14
Elema Samuel <99813035+elemasamuel@users.noreply.github.com>
2022-12-01 03:36:02 +0100

    Add files via upload

---
+++ static/quizes/quiz.js
@@ -0,0 +1,169 @@
+console.log('hello world quiz')
+const url = window.location.href
+console.log(url)
+
+const quizBox = document.getElementById('content-question-28324')
+const scoreBox = document.getElementById('score-box')
+const resultBox = document.getElementById('result-box')
+const timerBox = document.getElementById('timer-box')
+
+const activateTimer = (time) => {
+    console.log(time)
+
+    if (time.toString().length < 2) {
+        timerBox.innerHTML = `<span class="progress-number"> 0${time}:00 </span>
+                                <span class="progress-label">
+                                Time remaining </span>`
+    } else {
+        timerBox.innerHTML = `<span class="progress-number"> ${time}:00 </span>
+                                <span class="progress-label">
+                                Time remaining </span>`
+    }
+
+    let minutes = time - 1
+    let seconds = 60
+    let displaySeconds
+    let displayMinutes
+
+    const timer = setInterval(() => {
+        seconds--
+        if (seconds < 0) {
+            seconds = 59
+            minutes--
+        }
+        if (minutes.toString().length < 2) {
+            displayMinutes = '0' + minutes
+        } else {
+            displayMinutes = minutes
+        }
+        if (seconds.toString().length < 2) {
+            displaySeconds = '0' + seconds
+        } else {
+            displaySeconds = seconds
+        }
+        if (minutes === 0 && seconds === 0) {
+            timerBox.innerHTML = `<span class="progress-number"> 00:00 </span>
+                                <span class="progress-label">
+                                Time remaining </span>`
+            setTimeout(() => {
+                clearInterval(timer)
+                alert('Time over')
+                sendData()
+            }, 500)
+
+        }
+
+        timerBox.innerHTML = `<span class="progress-number"> ${displayMinutes}:${displaySeconds} </span>
+                                                                <span class="progress-label">
+                                                                Time remaining </span>`
+    }, 1000)
+}
+
+$.ajax({
+    type: 'GET',
+    url: `${url}data`,
+    success: function (response) {
+        // console.log(response)
+        const data = response.data
+        data.forEach(el => {
+            for (const [question, answers] of Object.entries(el)) {
+                quizBox.innerHTML += `
+                    <hr>
+                    <h4 class="question-title">${question}</h4>
+                `
+                answers.forEach(answer => {
+                    quizBox.innerHTML += `
+                        <ul id="answer-options-28324" class="answer-options">
+                            <li class="answer-option">
+                                <input type="radio" class="option-check" id="${question}-${answer}" name="${question}" value="${answer}" />
+                                <label for="${question}">${answer}</label>
+                            </li>
+                        </ul>
+                    `
+                })
+            }
+        });
+        activateTimer(response.time)
+
+    },
+    error: function (error) {
+        console.log(error)
+    }
+})
+
+
+const quizForm = document.getElementById('quiz-form')
+const csrf = document.getElementsByName('csrfmiddlewaretoken')
+
+
+const sendData = () => {
+    const elements = [...document.getElementsByClassName('option-check')]
+    const data = {}
+    data['csrfmiddlewaretoken'] = csrf[0].value
+    elements.forEach(el => {
+        if (el.checked) {
+            data[el.name] = el.value
+        } else {
+            if (!data[el.name]) {
+                data[el.name] = null
+            }
+        }
+    })
+
+    $.ajax({
+        type: 'POST',
+        url: `${url}save/`,
+        data: data,
+        success: function (response) {
+            // console.log(response)
+            const results = response.results
+            console.log(results)
+            quizForm.classList.add('not-visible')
+            timerBox.classList.add('not-visible')
+
+            scoreBox.innerHTML = `${response.passed ? 'Congratulations! ' : 'Oops..:( '}Your result is ${response.score.toFixed(2)}%`
+
+            results.forEach(res => {
+                const resDiv = document.createElement("div")
+                for (const [question, resp] of Object.entries(res)) {
+                    // console.log(question)
+                    // console.log(resp)
+                    // console.log('***')
+
+                    resDiv.innerHTML += question
+                    const cls = ['container', 'p-3', 'text-light', 'h3']
+                    resDiv.classList.add(...cls)
+
+                    if (resp == 'not answered') {
+                        resDiv.innerHTML += '- not answered'
+                        resDiv.classList.add('bg-danger')
+                    }
+                    else {
+                        const answer = resp['answered']
+                        const correct = resp['correct_answer']
+
+                        if (answer == correct) {
+                            resDiv.classList.add('bg-success')
+                            resDiv.innerHTML += ` answered: ${answer}`
+                        } else {
+                            resDiv.classList.add('bg-danger')
+                            resDiv.innerHTML += ` | correct answer: ${correct}`
+                            resDiv.innerHTML += ` | answered: ${answer}`
+                        }
+                    }
+                }
+                // const body = document.getElementsByTagName('BODY')[0]
+                resultBox.append(resDiv)
+            })
+        },
+        error: function (error) {
+            console.log(error)
+        }
+    })
+}
+
+quizForm.addEventListener('submit', e => {
+    e.preventDefault()
+
+    sendData()
+})
